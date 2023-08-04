cac488706d03c283d160ef80cc74e7feb11b4ba8
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-04 15:03:28 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+
+let requestsCount = 0;
+const server = http.createServer((request, response) => {
+
+  requestsCount++
+
+  switch (request.url) {
+    case '/':
+      response.write('MAIN PAGE')
+      break;
+    case '/students':
+      response.write('STUDENTS')
+      break
+    case '/courses':
+      response.write('FRONT + BACK')
+      break
+    default:
+      response.write('404 not found')
+
+  }
+  response.write('IT-ONLINE-SCHOOL ' + requestsCount)
+  response.end()
+})
+
+server.listen(3003)
