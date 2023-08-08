2f3b9577d164fabd4bed9c390a5b48ec0ccf3a4b
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-08 20:08:41 +0300

    commit async await

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
 const http = require('http')
+const fs = require('fs')
 
-// let requestsCount = 0;
-const server = http.createServer((request, response) => {
 
-  // requestsCount++
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+   setTimeout(() => {
+     resolve()
+   }, ms)
+  })
+}
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+  })
+  })
+}
+
+
+const server = http.createServer(async (request, response) => {
 
   switch (request.url) {
-    case '/':
-      response.write('MAIN PAGE')
+    case '/home': {
+      try {
+        const data = await readFile('pages/about.html')
+        response.write(data);
+        response.end()
+      }
+     catch(err) {
+       response.write('500 STRANGE ERROR');
+       response.end()
+     }
       break;
-    case '/students':
-      response.write('STUDENTS')
-      break
-    case '/courses':
-      response.write('FRONT + BACK')
-      break
-    default:
+    }
+    case '/about': {
+      await delay(3000)
+        response.write('ABOUT NODE.JS')
+        response.end()
+      break;
+    }
+    default: {
       response.write('404 not found')
+      response.end()
+    }
 
   }
-  // response.write('IT-ONLINE-SCHOOL ' + requestsCount)
-  response.end()
+
 })
 
 server.listen(3003)
